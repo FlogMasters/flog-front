1235473edf862e8c05f74fa73d67fec28235bfd5
NBJ1995 <darkmagic112@naver.com>
2020-09-29 17:14:24 +0900

    [FLOG-23] webpack 설정에 hot 추가

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const override = (config, env) => {
+  config.devServer = { ...config.devServer, hot: true }
   config.resolve.modules = [
     ...(config.resolve.modules || []),
     path.resolve(__dirname, 'src')
